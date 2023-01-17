39511a7736ace5d371af038efc2a0802a808a543
Jonathan Kuhn <jkuhn96@gmail.com>
2023-01-17 01:37:04 -0500

    EoS 5. Added register component and connected to account services

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { User } from './_models/user';
 import { AccountService } from './_services/account.service';
@@ -10,22 +9,11 @@ import { AccountService } from './_services/account.service';
 })
 export class AppComponent implements OnInit {
   title = 'The Dating App';
-  users: any;
 
-  constructor(private http: HttpClient, private accountService: AccountService) {}
+  constructor(private accountService: AccountService) {}
   
   ngOnInit() {
-    this.getUsers();
     this.setCurrentUser();
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe(response => {
-      this.users = response;
-      console.log(this.users);
-    }, error => {
-      console.log(error);
-    })
   }
 
   setCurrentUser() {
