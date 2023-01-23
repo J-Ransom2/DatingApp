8b3212bab0ce78547e7e4ca9ee45c300a78392b1
Jonathan Kuhn <jkuhn96@gmail.com>
2023-01-22 21:31:53 -0500

    added http interceptor for central error handling

--- client/src/app/_guards/auth.guard.ts
+++ client/src/app/_guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -9,7 +9,11 @@ import { AccountService } from '../_services/account.service';
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private accountService: AccountService, private toastr: ToastrService) {}
+  constructor(
+      private accountService: AccountService,
+      private toastr: ToastrService,
+      private router: Router
+    ) {}
 
   canActivate(): Observable<boolean> {
     return this.accountService.currentUser$.pipe(
@@ -17,6 +21,7 @@ export class AuthGuard implements CanActivate {
         if (user) return true;
         else {
           this.toastr.error('You shall not pass!!!');
+          this.router.navigateByUrl('/');
           return false;
         }
       })
